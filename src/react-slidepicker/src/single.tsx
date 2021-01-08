0ab7ea47ca34c73a3f753cfc77056976d10a54fb
xuwei <starkcoder@sina.com>
2021-01-08 18:36:43 +0800

    params

--- src/react-slidepicker/src/single.tsx
+++ src/react-slidepicker/src/single.tsx
@@ -1,7 +1,7 @@
 /*
  * @Author: xuwei
  * @Date: 2021-01-08 10:41:24
- * @LastEditTime: 2021-01-08 11:17:00
+ * @LastEditTime: 2021-01-08 18:03:09
  * @LastEditors: xuwei
  * @Description:
  */
@@ -26,6 +26,14 @@ interface Props {
 }
 
 export function SingleSlide(props: Props) {
+  function onStart(event: React.TouchEvent) {
+    console.info("START", event.touches[0].pageY);
+  }
+  function onMoving(event: React.TouchEvent) {
+    console.info("e", event);
+    console.info("START", event.touches[0].pageY);
+  }
+  function onMoveEnd() {}
   const {
     list,
     itemHeight,
@@ -36,9 +44,23 @@ export function SingleSlide(props: Props) {
   } = props;
   return (
     <div>
-      <div>
+      <div
+        onTouchMove={onMoving}
+        onTouchStart={onStart}
+        style={{ transform: "translateY(100px)" }}
+      >
         {[1, 1, 1, 11, 1].map((item, index) => (
-          <div style={{ height: itemHeight, backgroundColor: "#a00" }}></div>
+          <div
+            key={index}
+            style={{
+              width: 100,
+              height: itemHeight,
+              marginTop: 1,
+              backgroundColor: "#a00",
+            }}
+          >
+            {index}
+          </div>
         ))}
       </div>
     </div>
