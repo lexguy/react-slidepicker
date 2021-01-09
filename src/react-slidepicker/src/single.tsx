5dcf5758a854e54fc2106de55e030fd546e42090
xuwei <starkcoder@sina.com>
2021-01-09 18:34:01 +0800

    gesture move

--- src/react-slidepicker/src/single.tsx
+++ src/react-slidepicker/src/single.tsx
@@ -1,11 +1,11 @@
 /*
  * @Author: xuwei
  * @Date: 2021-01-08 10:41:24
- * @LastEditTime: 2021-01-08 18:03:09
+ * @LastEditTime: 2021-01-09 17:24:27
  * @LastEditors: xuwei
  * @Description:
  */
-import React from "react";
+import React, { useState } from "react";
 
 interface Props {
   list: object[];
@@ -25,15 +25,31 @@ interface Props {
   done: (a: number, b: number) => void;
 }
 
+let initOff = 0;
+let wrapOffset = 0; //每次手势结束后计算 div 基于初始位置的偏移
+
 export function SingleSlide(props: Props) {
+  const [offsetY, setOffsetY] = useState(0);
   function onStart(event: React.TouchEvent) {
-    console.info("START", event.touches[0].pageY);
+    const touchY = event.touches[0].pageY;
+    initOff = touchY;
+    console.info("Init", initOff);
   }
   function onMoving(event: React.TouchEvent) {
-    console.info("e", event);
-    console.info("START", event.touches[0].pageY);
+    const touchY = event.touches[0].pageY;
+    const transY = touchY - initOff + wrapOffset;
+    setOffsetY(transY);
+    console.info("-------------------------------");
+    console.info("ING--touchY", touchY);
+    console.info("ING--initOff", initOff);
+    console.info("ING--transY", transY);
   }
-  function onMoveEnd() {}
+  function onMoveEnd(event: React.TouchEvent) {
+    const touchY = event.changedTouches[0].pageY;
+    wrapOffset = wrapOffset + (touchY - initOff);
+    console.info("END", event);
+    console.info("wrapOffset", wrapOffset);
+  }
   const {
     list,
     itemHeight,
@@ -47,7 +63,8 @@ export function SingleSlide(props: Props) {
       <div
         onTouchMove={onMoving}
         onTouchStart={onStart}
-        style={{ transform: "translateY(100px)" }}
+        onTouchEnd={onMoveEnd}
+        style={{ transform: `translateY(${offsetY}px)` }}
       >
         {[1, 1, 1, 11, 1].map((item, index) => (
           <div
